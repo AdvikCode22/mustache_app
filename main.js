efd2c25498f1c6026a60fac5aff8fb4ac7d55b4a
AdvikCode22 <94784477+AdvikCode22@users.noreply.github.com>
2022-12-16 17:33:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    myCanvas = createCanvas(400,400)
+    myCanvas.center()
+}
+
+function draw(){
+
+}
+
+function capture(){
+    save("moustache image.png")
+}
